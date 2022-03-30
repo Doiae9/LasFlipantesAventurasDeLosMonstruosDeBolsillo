4eb8b4ed2b9551bab04d996d26687b7e1e44d5f8
Doiae9 <leonardogh75@gmail.com>
2022-03-30 12:00:40 -0600

    Construcción de tienda, clase objeto creada

---
+++ src/Tienda.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Tienda {
+    //nombre- String
+    //atributos
+    //objetos disponibles- ArrayList<objetos>
+
+    private String nombre;
+    private ArrayList<Objeto> objetosDisponibles;
+    //metodo
+
+    public boolean usuariocomprar(double dinero, int cantidad, int indiceObjeto){
+        //validar que hay suficientes objetos del tipo requerido
+        //Si hay cantidad y Objeto
+        //Si hay cantidad y existe en el arreglo
+        //Si si tengo, validar que el dinero sea suficiente
+        //Si es suficiente, le vendo
+        //Si no
+        //mostrar que no le alcanza
+        //sino
+        //hacemos excepción de que no hay la cantidad que pide
+        //repetimos pregunta
+            return false;
+        //
+
+
+    }
+    //comprar
+    //vender
+    public boolean usuariovender(Objeto objeto, int cantidad){
+        //logica inversa de compra
+        //dinero infinito en la tienda
+        //recibir objetos y darle el dinero
+return false;
+    }
+
+}
