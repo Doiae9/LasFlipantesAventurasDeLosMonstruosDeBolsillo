3f9e7982c3ac97f6fa421eabd21b3fa55320cb46
Doiae9 <leonardogh75@gmail.com>
2022-04-10 14:09:55 -0500

    Implementación de código, colocación de constructores, getters y Setters Baya, Habilidad pocion

--- src/Pocion.java
+++ src/Pocion.java
@@ -1,8 +1,31 @@
 public class Pocion extends Objeto {
     //efecto - String
     //duracion - entero
+    private String efecto;
+    private int duracion;
 
-    //usar
+    public Pocion(double costo, int cantidad, String nombre, String tipo, String efecto, int duracion) {
+        super(costo, cantidad, nombre, tipo);
+        this.efecto = efecto;
+        this.duracion = duracion;
+    }
+
+    public String getEfecto() {
+        return efecto;
+    }
+
+    public void setEfecto(String efecto) {
+        this.efecto = efecto;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+//usar
     //si es curacion 20 hp
     // si es fuerza
        //aumentar 20 ataque
