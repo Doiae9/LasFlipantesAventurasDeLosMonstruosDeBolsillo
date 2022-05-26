be2dec1704f371191fc2b44e4a914770b50efaa7
Doiae9 <leonardogh75@gmail.com>
2022-05-26 11:31:45 -0500

    Commit Avance 5 26.04.2022 creacion de interface Aleatorio

---
+++ src/UtilInterface.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public interface UtilInterface {
+    //funcion que regrea el tiempo actual en milisegundos
+    void tiempoJugado(long tiempoInicial, long tiempoFinal);
+    void mostrarMenu();
+    Pokemon crearPokemonAleatorio();
+    //GenericoAleatorio
+    <T> T sacarAleatorio(ArrayList<T> arrayList);
+    //String[] Debil y fuerte
+    String[] debilYFuerteAleatorio(String tipo);
+
+    //String[] Nombres Pokemon = devolver arreglo de string
+    //Colocar los tipos en un switch
+    String[] nombresAleatorios(String tipo);
+}
